65cce3c37eedf8f251c2482ade813f8e52f42f58
Esa <esaruhulla01@gmail.com>
2025-11-10 10:09:10 +0600

    initial setup done

---
+++ src/Components/Home/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const bannder = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default bannder;
---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+import { Outlet } from 'react-router';
+
+const MainLayout = () => {
+    return (
+        <div className='flex flex-col min-h-screen'>
+            <Header></Header>
+
+            <div className='flex-1 w-[1820px] mx-auto'>
+                <Outlet></Outlet>
+            </div>
+
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainLayout;
